1abeadf3ee834c6413cef862fbcc5510500276e4
splashx <splash@gmail.com>
2014-10-30 23:32:31 +0100

    working

--- url_cat_scrapper.py
+++ url_cat_scrapper.py
@@ -25,77 +25,75 @@ parser.add_argument("-p", "--proxy-list", dest="proxyList", help="proxy list fil
 parser.add_argument("-c", "--captcha", dest="captchaCode", help="The value of CaptchaCode", required=False)
 parser.add_argument("-s", "--session-id", dest="sessionID", help="The value of LBD_VCID_LoginCaptcha", required=False)
 
+"""" func """
+def fetchCategory(proxy, urllist, urlscategorized):
+	try:
+		url = urllist.pop()
+	except:
+		print "urllist empty - nothing to do. Returning.."
+		return
+	
+	if url in urlscategorized:
+		print "[*] URL already categorized. Doing nothing..."
+		return 1 #nothing to do, URL is already categorized, success
+	else:
+		try:
+			print url + ": retrieving category via  " + proxy + " ..."
+			cj = cookielib.CookieJar()
+			proxy_handler = urllib2.ProxyHandler({'http': 'http://' + proxy})
+			opener = urllib2.build_opener(proxy_handler, urllib2.HTTPCookieProcessor(cj))
+			user_agent = random.choice(open(USER_AGENT_FILE).readlines()).strip()
+			opener.addheaders = [('User-agent', user_agent)]
+
+			req = opener.open("http://www.fortiguard.com/ip_rep/index.php?data=" + url + "?", None, timeout=TIMEOUT)
+			html_response = req.read()
+		
+			regex = '<h3 style="float: left">Category: ([a-zA-Z\- ]+)</h3>'
+			reg_match = re.search(regex, html_response, re.MULTILINE)
+			print url,
+			print reg_match
+			if reg_match:
+				category = reg_match.group(1)
+				print "[*] Found! " + url + " = " + category
+ 				urlscategorized[url] = category
+				return 1
+			else:   #couldn't match, assuming proxy issue
+				urllist.append(url)
+				return 0
+		
+			print "["  +  str(n) + "] proxy = " + proxy + "\t url = " + url  + "\tmessage = " + category
+				
+		except Exception, err:
+				urllist.append(url)
+				print "exception["  +  str(n) + "] proxy = " + proxy + "\t url = " + url  + "\tmessage = " + str(err)
+				return 0
+				
+				
+""" main """
 args = parser.parse_args()
 pickled_file = args.urlList + ".pickled"
 
 TIMEOUT = 10
 USER_AGENT_FILE = 'user-agent_list.txt'
 
-"""" func """
-def fetchCategory(proxy, urllist, urlscategorized):
-	print "Entered fetchCategory1 for " + proxy
-	cj = cookielib.CookieJar()
-	proxy_handler = urllib2.ProxyHandler({'http': 'http://' + proxy})
-	opener = urllib2.build_opener(proxy_handler, urllib2.HTTPCookieProcessor(cj))
-	user_agent = random.choice(open(USER_AGENT_FILE).readlines()).strip()
-	opener.addheaders = [('User-agent', user_agent)]
-
-	for n in range(0,3):
-		url = urllist.pop()
-		
-		if not url:
-			return 1; #nothing to do, list is empty, success
-		else:
-			if url is in urlscategorized:
-				return 1 #nothing to do, URL is already categorized, success
-			else:
-				try:
-						req = opener.open("http://www.fortiguard.com/ip_rep.php?data=" + url, None, timeout=TIMEOUT)
-						html_response = req.read()
-					
-						regex = '<h3 style="float: left">Category: ([a-zA-Z ]+)</h3>'
-						reg_match = re.search(regex, html_response, re.MULTILINE)
-						if reg_match:
-							category = reg_match.group(1)
-							urlscategorized[url] = category
-							return 1
-						else:   #couldn't match, assuming proxy issue
-							print "Appending url"
-							urllist.append(url)
-							return 0
-					
-						print "["  +  str(n) + "] proxy = " + proxy + "\t url = " + url  + "\tmessage = " + category
-						
-				except Exception, err:
-						urllist.append(url)
-						print "exception["  +  str(n) + "] proxy = " + proxy + "\t url = " + url  + "\tmessage = " + str(err)
-						return 0
-				
 manager = multiprocessing.Manager()
 urlList = manager.list()
 proxyQueue = Queue()
 restoredDict = dict()
 
 try:
-	f=open(pickled_file, 'rb')
-	while 1:
-		try:
-			temp_dict= pickle.load(f)[0]
-			print "loaded: ",
-			print temp_dict
-			restoredDict.update(temp_dict)
-			print "In the while.."
-		except EOFError:
-			print "EOFerror"
-			break
-		except Exception, err:
-			print "General Except" + str(err)
-			break
-	f.close()
-	urlsCategorized = manager.dict(restoredDict)
+	with open( args.urlList, 'rb') as h:
+		for line in h:
+		    li=line.strip()
+		    if not li.startswith("#"):
+		    	urlList.append(li)
 except:
-	print "[*] Unable to load " + pickled_file + ". That's okay if it's your first run.."
-	urlsCategorized = manager.dict()
+	print "\nError on processing " + str(args.urlList) + ". Exiting..."
+	exit()
+
+if not urlList:
+	print "[*] " + str(args.urlList) + " is empty. Exiting.."
+	exit()
 
 try:
 	with open( args.proxyList, "rb") as g:
@@ -103,25 +101,37 @@ try:
 		    li=line.strip()
 		    if not li.startswith("#"):
 		    	proxyQueue.put(li)
-
 except:
 	print "\nError on processing " + str(args.proxyList) + ". Exiting..."
 	exit()
-try:
-	with open( args.urlList, 'rb') as h:
-		for line in h:
-		    li=line.strip()
-		    if not li.startswith("#"):
-		    	urlList.append(li)
 
-except:
-	print "\nError on processing " + str(args.urlList) + ". Exiting..."
+if proxyQueue.empty():
+	print "[*] " + str(args.proxyList) + " is empty. Exiting.."
 	exit()
-    	
+
+try:
+	f=open(pickled_file, 'rb')
+	while 1:
+		try:
+			temp_dict= pickle.load(f)[0]
+			restoredDict.update(temp_dict)
+		except EOFError:
+			break
+		except Exception, err:
+			print "General Except" + str(err)
+			break
+	f.close()
+	urlsCategorized = manager.dict(restoredDict)
+except:
+	print "[*] Unable to load " + pickled_file + " (okay for a first run)"
+	urlsCategorized = manager.dict()
+
 pool = multiprocessing.Pool(multiprocessing.cpu_count()*10)
-while not proxyQueue.empty() or not urlList:
-	proxy_candidate = proxyQueue.get()
-	pool.apply_async(fetchCategory, args=(proxy_candidate, urlList, urlsCategorized,))
+while not proxyQueue.empty():
+	if urlList:
+		proxy_candidate = proxyQueue.get()
+		for n in range(0,10):
+			pool.apply_async(fetchCategory, args=(proxy_candidate, urlList, urlsCategorized,))
 pool.close()
 pool.join()	
 
@@ -130,9 +140,8 @@ backupDict = dict(urlsCategorized)
 # pickle urlsCategorized
 try:
 		pickledData = [backupDict]
-		with open( pickled_file, "ab" ) as f:
+		with open( pickled_file, "wb" ) as f:
     			pickle.dump( pickledData, f)
-		print "[*] Successfully pickled to " + pickled_file
 except:
 		print "[*] Bummer - couldn't pickle. Resume won't be possible (this is weird..)"
 
@@ -144,11 +153,6 @@ try:
 		print "[*] Successfully updated " + str(args.urlList)
 except:
 		print "[*] Error while updating " + str(args.urlList)
-		
-print backupDict
 
 manager.shutdown()
 del manager
-
-
-
